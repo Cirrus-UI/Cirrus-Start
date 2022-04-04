654004e1e9de4303d0f3823a0347510e2396c2b7
Stanley Lim <slim679975@gmail.com>
2022-04-03 23:59:25 -0700

    :sparkles: feat(webpack.config.js): Add Purge CSS support

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const glob = require('glob');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const PurgeCSSPlugin = require('purgecss-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const BUILD_DIR = './dist';
 
@@ -30,7 +32,18 @@ module.exports = {
         rules: [
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
+                // use: ['style-loader', 'css-loader', 'sass-loader'],
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: false,
+                            sourceMap: true,
+                        },
+                    },
+                    'sass-loader', // compiles Sass to CSS, using Node Sass by default
+                ],
             },
             {
                 test: /\.js$/,
@@ -47,17 +60,44 @@ module.exports = {
     plugins: [
         new webpack.ProgressPlugin(),
         new CleanWebpackPlugin(),
-        new HTMLWebpackPlugin({ template: './src/index.html' }),
+        new MiniCssExtractPlugin({
+            filename: '[name]-[contenthash].css',
+        }),
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            minify:
+                process.env.NODE_ENV === 'production'
+                    ? {
+                          collapseWhitespace: true,
+                          removeComments: true,
+                          removeRedundantAttributes: true,
+                          removeScriptTypeAttributes: true,
+                          removeStyleLinkTypeAttributes: true,
+                          useShortDoctype: true,
+                      }
+                    : false,
+            template: 'src/index.html',
+        }),
         new PurgeCSSPlugin({
-            paths: glob.sync(`src/**/*`, { nodir: true }),
+            paths: glob.sync(path.join(__dirname, 'src') + '/**/*', { nodir: true }),
         }),
     ],
     optimization: {
+        minimizer: [new CssMinimizerPlugin()],
+        minimize: true, // Css minmizer config
         runtimeChunk: {
             name: 'manifest',
         },
         splitChunks: {
             chunks: 'all',
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    type: 'css/mini-extract',
+                    chunks: 'all',
+                    enforce: true,
+                },
+            },
         },
     },
 };
